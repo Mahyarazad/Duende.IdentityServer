d4b94940db5e22a166f374ab84344c1c77b9e3b5
Maahyar Azad <mhyrinc@gmail.com>
2024-07-13 12:34:56 +0330

    Add project files.

---
+++ ImageGallery.API/Authorization/MustOwnImageHandler.cs
@@ -0,0 +1,47 @@
+﻿using ImageGallery.API.Services;
+using Microsoft.AspNetCore.Authorization;
+using System.Runtime.InteropServices;
+
+namespace ImageGallery.API.Authorization
+{
+    public class MustOwnImageHandler : AuthorizationHandler<MustOwnImageRequirement>
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+        private readonly IGalleryRepository _galleryRepository;
+
+        public MustOwnImageHandler(IHttpContextAccessor contextAccessor, IGalleryRepository galleryRepository)
+        {
+            _contextAccessor = contextAccessor ?? throw new NullReferenceException(nameof(contextAccessor));
+            _galleryRepository = galleryRepository ?? throw new NullReferenceException(nameof(galleryRepository)); ;
+        }
+
+        protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, MustOwnImageRequirement requirement)
+        {
+            var imageId = _contextAccessor.HttpContext!.GetRouteValue("id")!.ToString();
+
+            if(!Guid.TryParse(imageId, out Guid res))
+            {
+                context.Fail();
+                return;
+            }
+
+            var ownerId = context.User.Claims.FirstOrDefault(x => x.Type == "sub")?.Value;
+
+            if(ownerId == null)
+            {
+                context.Fail();
+                return;
+            }
+
+            var image = _galleryRepository.GetImageAsync(res);
+            if (!await _galleryRepository.IsImageOwnerAsync(res, ownerId))
+            {
+                context.Fail();
+                return;
+            }
+
+            context.Succeed(requirement);
+            return;
+        }
+    }
+}
---
+++ Marvin.IDP/Config.cs
@@ -0,0 +1,71 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+
+namespace Marvin.IDP;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+            new IdentityResource("roles",
+                "Your role(s)",
+                new [] { "role" }),
+            new IdentityResource("country",
+                "Residency country",
+                new [] { "country" })
+        };
+
+    public static IEnumerable<ApiResource> ApiResources => new ApiResource[] {
+        new ApiResource
+        {
+            Name = "imagegalleryapi",
+            DisplayName = "Image Gallery Api",
+            UserClaims = new []
+            {
+                "role", "country"
+            },
+            Scopes = { "imagegalleryapi.read", "imagegalleryapi.write", "imagegalleryapi.fullaccess" }
+        }
+    };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+            {
+                new("imagegalleryapi.fullaccess"),
+                new("imagegalleryapi.write"),
+                new("imagegalleryapi.read"),
+            };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[]
+            {
+                new Client()
+                {
+                    ClientName = "Image Gallery",
+                    ClientId = "imagegalleryclient",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris =
+                    {
+                        "https://localhost:7184/signin-oidc"
+                    },
+                    PostLogoutRedirectUris =
+                    {
+                        "https://localhost:7184/signout-callback-oidc"
+                    },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "roles", "imagegalleryapi.read", "imagegalleryapi.write", "country"
+                    },
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    RequireConsent = true
+                }
+            };
+}
